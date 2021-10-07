29ae5a3258ae9002318efbb377a1d2eaa55a960a
NavyaSaragagadam <navya26saragadam@gmail.com>
2021-10-07 06:34:47 -0700

    Display search value in heading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   currentSearch: Array<IFeatureShows> = []
   todaysShows: Array<IFeatureShows> = []
   displayTodayShows = true;
+  currentSearchValue = "";
     
     constructor(private showsService: ShowsService) {
       this.getTodaysShows()
@@ -20,6 +21,7 @@ export class AppComponent {
     
     doSearch(searchValue:string) {
       const userInput = searchValue.split(',').map(i => i.trim() );
+      this.currentSearchValue = userInput[0];
       
       this.showsService.getShows(userInput[0]).subscribe(data => this.currentSearch = data)
       this.displayTodayShows = false;
